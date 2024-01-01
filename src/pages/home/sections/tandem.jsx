255bb4c6a6ebdb57d4bfebf237a67524e7e1a0da
mutua-muvevi <samstore637@gmail.com>
2024-01-01 23:28:31 +0300

    title subtitle component

--- src/pages/home/sections/tandem.jsx
+++ src/pages/home/sections/tandem.jsx
@@ -1,5 +1,19 @@
+import { Stack } from "@mui/material";
+import TitleSubtitle from "../../../modules/title-subtitle";
+
 const HomeTandem = () => {
-	return <div>HomeTandem</div>;
+	return (
+		<Stack sx={{p:20}}>
+			<Stack>
+				<TitleSubtitle
+					title="Tandem Skydiving"
+					subtitle="The best way to experience skydiving"
+					position="left"
+				/>
+			</Stack>
+			<Stack>Lower</Stack>
+		</Stack>
+	);
 };
 
 export default HomeTandem;
