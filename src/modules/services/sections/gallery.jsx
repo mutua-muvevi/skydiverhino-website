3750c6bdc43d91b4e04447db81366202c9f9e222
mutua-muvevi <samstore637@gmail.com>
2024-01-03 00:12:15 +0300

    made the servic reusable

--- src/modules/services/sections/gallery.jsx
+++ src/modules/services/sections/gallery.jsx
@@ -1,56 +1,51 @@
-import { Card, CardActionArea, Dialog, Stack } from "@mui/material";
+import { Card, CardActionArea, Dialog, Stack, useTheme } from "@mui/material";
 import TitleSubtitle from "../../title-subtitle";
 import { Masonry } from "@mui/lab";
 import { Container } from "@mui/system";
 import { useState } from "react";
+import PropTypes from "prop-types";
 
-const images = [
-	"https://res.cloudinary.com/dqweh6zte/image/upload/v1692719152/officechest/services/ppqibj8mpvumxmzj7kqb.jpg",
-	"https://res.cloudinary.com/dqweh6zte/image/upload/v1693131018/Officechest/users/bgbyh9316o4seul02hgp.jpg",
-	"https://res.cloudinary.com/dqweh6zte/image/upload/v1690303441/officechest/services/jog7skpaltedt5a3eips.jpg",
-	"https://res.cloudinary.com/dqweh6zte/image/upload/v1693131018/Officechest/users/bgbyh9316o4seul02hgp.jpg",
-	"https://res.cloudinary.com/dqweh6zte/image/upload/v1690303441/officechest/services/jog7skpaltedt5a3eips.jpg",
-	"https://res.cloudinary.com/dqweh6zte/image/upload/v1692719152/officechest/services/ppqibj8mpvumxmzj7kqb.jpg",
-	"https://res.cloudinary.com/dqweh6zte/image/upload/v1692640097/officechest/services/ezqt2maojaypqh0movdm.png",
-	"https://res.cloudinary.com/dqweh6zte/image/upload/v1679749553/skydive%20rhino/images/IMG_20230126_072246_564_voxwjd.jpg",
-	"https://res.cloudinary.com/dqweh6zte/image/upload/v1679650179/skydive%20rhino/images/IMG_20230124_144004_883_xa4ruw.jpg",
-	"https://res.cloudinary.com/dqweh6zte/image/upload/v1673047324/skydive%20rhino/images/skydiving-gd90b87829_1920_ztat2b.jpg",
-
-];
-
-const ServiceGallery = () => {
+const ServiceGallery = ({ gallery }) => {
 	const [open, setOpen] = useState(false);
 	const [selectedImage, setSelectedImage] = useState(null);
+	const theme = useTheme();
 
 	const handleOpen = (image) => {
 		console.log("open");
 		setSelectedImage(image);
 		setOpen(true);
-	}
+	};
 	return (
 		<div>
-			<Container maxWidth="xl" sx={{py: 10}}>
+			<Container maxWidth="xl" sx={{ py: 10 }}>
 				<Stack direction="column" spacing={3}>
-					<TitleSubtitle title="Gallery" subtitle="Our gallery" />
+					<TitleSubtitle
+						title={gallery.title}
+						subtitle={gallery.subtitle}
+						divider
+						dividerColor={theme.palette.text.primary}
+					/>
 					<div>
-							<Masonry columns={{ xs: 2, sm: 3, md: 4 }} spacing={2}>
-								{images.map((image, index) => (
-									<Card key={index}>
-										<CardActionArea onClick={() => handleOpen(image)}>
-											<img
-												src={image}
-												alt={`Image ${index + 1}`}
-												style={{
-													width: "100%",
-													height: "100%",
-													objectFit: "cover",
-												}}
-											/>
-										</CardActionArea>
-									</Card>
-								))}
-							</Masonry>
-						</div>
+						<Masonry columns={{ xs: 2, sm: 3, md: 4 }} spacing={2}>
+							{gallery.images.map((image, index) => (
+								<Card key={index}>
+									<CardActionArea
+										onClick={() => handleOpen(image)}
+									>
+										<img
+											src={image}
+											alt={`Image ${index + 1}`}
+											style={{
+												width: "100%",
+												height: "100%",
+												objectFit: "cover",
+											}}
+										/>
+									</CardActionArea>
+								</Card>
+							))}
+						</Masonry>
+					</div>
 				</Stack>
 			</Container>
 
@@ -71,6 +66,10 @@ const ServiceGallery = () => {
 			</Dialog>
 		</div>
 	);
+};
+
+ServiceGallery.propTypes = {
+	gallery: PropTypes.object,
 };
 
 export default ServiceGallery;
--- src/modules/services/sections/requirements.jsx
+++ src/modules/services/sections/requirements.jsx
@@ -12,45 +12,8 @@ import { useState } from "react";
 import Iconify from "../../../components/iconify";
 import PropTypes from "prop-types";
 
-const requirements = [
-	{
-		title: "Age Requirement",
-		content:
-			"Participants must be at least 18 years of age. This is a strict requirement, and valid photo identification will be required on the day of the jump to confirm your age.",
-	},
-	{
-		title: "Weight Limit",
-		content:
-			"There is typically a weight limit for tandem skydiving, usually between 220 to 250 pounds. This limit is for the safety of both the participant and the instructor. Additionally, participants should be within a healthy weight range for their height to ensure the harness fits securely.",
-	},
-	{
-		title: "Physical Fitness",
-		content:
-			"Participants should be in good physical health. While no specific fitness level is required, you should be able to lift your legs for landing. It's important to inform the skydiving center of any medical conditions or physical limitations beforehand.",
-	},
-	{
-		title: "Clothing and Footwear",
-		content:
-			"Wear comfortable, weather-appropriate clothing that fits well under a jumpsuit. Shoes should be closed-toed and secure on your feet – no sandals, flip-flops, or heels. Sneakers or athletic shoes are ideal.",
-	},
-	{
-		title: "Training Session",
-		content:
-			"All tandem jumpers are required to complete a brief training session before the jump. This training covers essential information about the jump process, safety procedures, and what to expect during freefall and landing.",
-	},
-	{
-		title: "Health Considerations",
-		content:
-			"Inform the skydiving center if you have any health issues such as heart conditions, epilepsy, or previous injuries. Certain conditions may prevent you from safely skydiving. Always consult with a healthcare provider if unsure.",
-	},
-	{
-		title: "Alcohol and Drugs",
-		content:
-			"You must not be under the influence of alcohol or drugs. These substances can impair your judgment and reflexes, posing a significant safety risk. Most centers will refuse to let you jump if they suspect impairment.",
-	},
-];
 
-const ServiceRequirements = ({ backgroundImage }) => {
+const ServiceRequirements = ({  requirements }) => {
 	const theme = useTheme();
 	const [expanded, setExpanded] = useState(false);
 
@@ -58,7 +21,7 @@ const ServiceRequirements = ({ backgroundImage }) => {
 		setExpanded(isExpanded ? panel : false);
 	};
 
-	const summaryColor = backgroundImage ? "#fff" : theme.palette.text.primary;
+	const summaryColor = requirements.image ? "#fff" : theme.palette.text.primary;
 	const expandedColor = theme.palette.text.primary;
 
 	return (
@@ -66,12 +29,12 @@ const ServiceRequirements = ({ backgroundImage }) => {
 			<Container maxWidth="xl" sx={{ py: 10 }}>
 				<Stack direction="column" spacing={3}>
 					<TitleSubtitle
-						title="Requirements"
-						subtitle="What you need to get started"
+						title={requirements.title ? requirements.title : "Requirements"}
+						subtitle={requirements.subtitle ? requirements.subtitle : "The latest news from Skydive Rhino"}
 						position="left"
 					/>
 
-					{requirements.map((item, index) => (
+					{requirements.requirements.map((item, index) => (
 						<Accordion key={index} onChange={handleChange(index)}>
 							<AccordionSummary
 								aria-controls={`panel${index}a-content`}
@@ -106,7 +69,7 @@ const ServiceRequirements = ({ backgroundImage }) => {
 									fontWeight={500}
 									textAlign="justify"
 								>
-									{item.content}
+									{item.details}
 								</Typography>
 							</AccordionDetails>
 						</Accordion>
@@ -119,6 +82,7 @@ const ServiceRequirements = ({ backgroundImage }) => {
 
 ServiceRequirements.propTypes = {
 	backgroundImage: PropTypes.string,
+	requirements: PropTypes.object
 };
 
 export default ServiceRequirements;
